ddbbaf9e5ffac7619a5808add92cd2df9a971fc5
Edson Finda <edsonbaptistafinda@gmail.com>
2025-05-24 14:46:43 +0100

    updated

--- inc/tetr.h
+++ inc/tetr.h
@@ -6,7 +6,7 @@
 /*   By: efinda <efinda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 12:59:02 by efinda            #+#    #+#             */
-/*   Updated: 2025/05/23 13:05:58 by efinda           ###   ########.fr       */
+/*   Updated: 2025/05/24 14:08:43 by efinda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,22 @@
 # include <sys/time.h>
 # include <time.h>
 
+# define WIDTH 1920
+# define HEIGHT 1080
+
+# define TILE 16
+
+// INIT_TETR
+void		init_tetr(t_tetr *tetr);
+
+// DEALLOCATE_TETR
+void		deallocate_tetr(t_tetr *tetr, char *message, int status);
+
+// TILES
+extern void	swap_tile_attr(t_tile *a, t_tile *z);
+void		free_tiles(t_tile ***tiles, int rows);
+void		link_tiles(t_tile **tiles, t_point size);
+void		init_tiles(t_tile ***tiles, t_point size);
+void		set_tiles(t_tile **tiles, t_point size, t_point padd);
 
 #endif
--- inc/tetr_structs.h
+++ inc/tetr_structs.h
@@ -6,7 +6,7 @@
 /*   By: efinda <efinda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 12:58:58 by efinda            #+#    #+#             */
-/*   Updated: 2025/05/23 13:03:18 by efinda           ###   ########.fr       */
+/*   Updated: 2025/05/24 13:42:40 by efinda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,22 @@
 
 typedef struct s_tile
 {
-	char				dir;
-	int					color;
-	t_point				crd;
-	struct s_tile		*up;
-	struct s_tile		*down;
-	struct s_tile		*left;
-	struct s_tile		*right;
-}						t_tile;
+	int				color;
+	t_point			crd;
+	struct s_tile	*up;
+	struct s_tile	*down;
+	struct s_tile	*left;
+	struct s_tile	*right;
+}					t_tile;
 
 typedef struct s_tetr
 {
-	void	*mlx;
-	void	*win;
-	t_img	main_img;
-	t_img	background_img;
-    t_tile  **tiles;
-}			t_tetr;
+	void			*mlx;
+	void			*win;
+	t_img			main_img;
+	t_img			background_img;
+	t_tile			**tiles;
+	t_point			total_tiles;
+}					t_tetr;
 
 #endif
