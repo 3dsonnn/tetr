cc584a679f7889e5809ea2d962735716fc5f66bd
Edson Finda <edsonbaptistafinda@gmail.com>
2025-05-24 14:43:12 +0100

    function to deallocate all the resources used on the game

---
+++ src/deallocate_tetr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   deallocate_tetr.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: efinda <efinda@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/24 12:13:27 by efinda            #+#    #+#             */
+/*   Updated: 2025/05/24 14:02:45 by efinda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/tetr.h"
+
+void    deallocate_tetr(t_tetr *tetr, char *message, int status)
+{
+    if (status && message)
+        ft_dprintf(2, "Error!\n%s\n", message);
+    my_mlx_destroy_img(tetr->mlx, &tetr->main_img);
+    my_mlx_destroy_img(tetr->mlx, &tetr->background_img);
+    free_tiles(&tetr->tiles, tetr->total_tiles.y);
+    if (tetr->mlx && tetr->win)
+        mlx_destroy_window(tetr->mlx, tetr->win);
+    if (tetr->mlx)
+    {
+        mlx_destroy_display(tetr->mlx);
+        free(tetr->mlx);
+    }
+}
