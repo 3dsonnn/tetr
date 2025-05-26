1c740403ec612b6d22ff7de13303fe104bc043f0
Edson Finda <edsonbaptistafinda@gmail.com>
2025-05-26 19:40:51 +0100

    added t_points on the main structure to keep track of the hold and next boxes as well as the imbroglio bar

--- inc/tetr_structs.h
+++ inc/tetr_structs.h
@@ -6,7 +6,7 @@
 /*   By: efinda <efinda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 12:58:58 by efinda            #+#    #+#             */
-/*   Updated: 2025/05/26 08:14:57 by efinda           ###   ########.fr       */
+/*   Updated: 2025/05/26 17:36:13 by efinda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,20 @@
 # define YELLOW_ANSI "\x1b[33m"
 # define MAGENTA_ANSI "\x1b[35m"
 
-# define SKY	 "assets/sky.xpm"
-# define HOME    "assets/home.xpm"
-# define JPINA   "assets/jpina.xpm"
-# define OCEAN   "assets/ocean.xpm"
+# define SKY "assets/sky.xpm"
+# define HOME "assets/home.xpm"
+# define JPINA "assets/jpina.xpm"
+# define OCEAN "assets/ocean.xpm"
 # define BONFIRE "assets/bonfire.xpm"
 # define CLUSTER "assets/cluster.xpm"
+
+typedef struct s_plane
+{
+	int				x0;
+	int				x;
+	int				y0;
+	int				y;
+}					t_plane;
 
 typedef struct s_tile
 {
@@ -53,6 +61,9 @@ typedef struct s_tetr
 	t_img			main_img;
 	t_img			background_img;
 	t_tile			**tiles;
+	t_point			hold_box_crd;
+	t_point			next_box_crd;
+	t_plane			imbroglio_bar_crd;
 	t_player		player;
 }					t_tetr;
 
