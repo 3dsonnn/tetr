8d57ab5792caba3d445101ae31f90db3e770cdf0
Edson Finda <edsonbaptistafinda@gmail.com>
2025-05-26 19:37:47 +0100

    now the tiles crd is being correctly set, gaping a tile between them

--- src/tile/set.c
+++ src/tile/set.c
@@ -6,7 +6,7 @@
 /*   By: efinda <efinda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 15:28:03 by efinda            #+#    #+#             */
-/*   Updated: 2025/05/26 08:18:50 by efinda           ###   ########.fr       */
+/*   Updated: 2025/05/26 14:18:55 by efinda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	set_tiles(t_tile **tiles, t_point padd)
 {
 	t_point	iter;
+	t_tile	*tmp;
 
 	iter = (t_point){-1, -1};
 	if (!tiles || !(*tiles))
@@ -24,17 +25,16 @@ void	set_tiles(t_tile **tiles, t_point padd)
 		iter.x = -1;
 		while (++iter.x < TOTAL_TILE_X)
 		{
-			tiles[iter.y][iter.x].color = BLACK;
-			if (!iter.x)
-				tiles[iter.y][iter.x].crd = (t_point){
-					.x = padd.x + (iter.x * TILE),
-					.y = padd.y + (iter.y * TILE)
-				};
+			tmp = &tiles[iter.y][iter.x];
+			tmp->color = BLACK;
+			if (!iter.x && !iter.y)
+				tmp->crd = padd;
+			else if (!iter.x)
+				tmp->crd = (t_point){.x = padd.x,
+					.y = tmp->up->crd.y + TILE + 1};
 			else
-				tiles[iter.y][iter.x].crd = (t_point){
-					.x = (padd.x + (iter.x * TILE)) + 1,
-					.y = (padd.y + (iter.y * TILE)) + 1
-				};
+				tmp->crd = (t_point){.x = tmp->left->crd.x + TILE + 1,
+					.y = tmp->left->crd.y};
 		}
 	}
 }
