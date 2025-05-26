77a4c73c68a01cebd050f5a30bcad2e38aae2634
Edson Finda <edsonbaptistafinda@gmail.com>
2025-05-26 08:43:53 +0100

    just updated the code with the macros representing the amount of the tiles, since it's a constant value, I think it's better to be a macro instead of a variable inside the main structure

--- src/tile/init.c
+++ src/tile/init.c
@@ -6,32 +6,32 @@
 /*   By: efinda <efinda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 07:46:13 by efinda            #+#    #+#             */
-/*   Updated: 2025/05/24 13:46:20 by efinda           ###   ########.fr       */
+/*   Updated: 2025/05/26 08:16:47 by efinda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/tetr.h"
 
-void	init_tiles(t_tile ***tiles, t_point size)
+void	init_tiles(t_tile ***tiles)
 {
 	t_point	iter;
 
 	iter = (t_point){.x = -1, .y = -1};
-	if (!tiles || ft_origin(size))
+	if (!tiles)
 		return ;
-	*tiles = (t_tile **)malloc(sizeof(t_tile *) * size.y);
+	*tiles = (t_tile **)malloc(sizeof(t_tile *) * TOTAL_TILE_Y);
 	if (!(*tiles))
 		return ;
-	while (++iter.y < size.y)
+	while (++iter.y < TOTAL_TILE_Y)
 	{
-		(*tiles)[iter.y] = (t_tile *)malloc(sizeof(t_tile) * size.x);
+		(*tiles)[iter.y] = (t_tile *)malloc(sizeof(t_tile) * TOTAL_TILE_X);
 		if (!(*tiles)[iter.y])
 		{
 			free_tiles(tiles, iter.y);
 			return ;
 		}
 		iter.x = -1;
-		while (++iter.x < size.x)
+		while (++iter.x < TOTAL_TILE_X)
 			(*tiles)[iter.y][iter.x] = (t_tile){.color = 0, .crd = (t_point){0, 0},
 				.up = NULL, .down = NULL, .right = NULL, .left = NULL};
 	}
