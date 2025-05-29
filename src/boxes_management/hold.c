89b2cb952b0977d9762edb7a9efb3e57d095e2f2
Edson Finda <edsonbaptistafinda@gmail.com>
2025-05-29 17:18:12 +0100

    on this file I created the logic to hold pieces. I can put the current piece on hold and change to current to be the first one on the next box; it avoids holding a piece more than once at a time; and paints the piece on hold with gray when you until you can get it

--- src/boxes_management/hold.c
+++ src/boxes_management/hold.c
@@ -6,22 +6,44 @@
 /*   By: efinda <efinda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 20:34:44 by efinda            #+#    #+#             */
-/*   Updated: 2025/05/27 13:45:38 by efinda           ###   ########.fr       */
+/*   Updated: 2025/05/29 17:11:31 by efinda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/tetr.h"
 
-void    hod_piece(t_tetr *tetr, int id)
+static  void    first_time_holding(t_tetr *tetr, t_piece *holded_piece)
 {
-    static  int status;
+    *holded_piece = tetr->cur;
+    holded_piece->color = GRAY;
+    put_piece(*holded_piece, tetr->hold_box_size, &tetr->main_img);
+    holded_piece->color = tetr->cur.color;
+    update_piece(tetr);
+    tetr->hold_toggle = 0;
+}
 
-    if (status)
+void	hold_piece(t_tetr *tetr, int flag)
+{
+	static t_piece	holded_piece;
+    t_piece         tmp;
+
+    if (tetr->hold_toggle == -1)
+        first_time_holding(tetr, &holded_piece);
+    else if (flag)
     {
-
+        clean_box(tetr, &tetr->main_img, tetr->background_img, tetr->hold_box_size);
+        put_piece(holded_piece, tetr->hold_box_size, &tetr->main_img);
+        tetr->hold_toggle = 1;
     }
-    else
+    else if (tetr->hold_toggle == 1)
     {
-        
+        tmp = holded_piece;
+        holded_piece = tetr->cur;
+        holded_piece.color = GRAY;
+        clean_box(tetr, &tetr->main_img, tetr->background_img, tetr->hold_box_size);
+        put_piece(holded_piece, tetr->hold_box_size, &tetr->main_img);
+        holded_piece.color = tetr->cur.color;
+        tetr->cur = tmp;
+        tetr->hold_toggle = 0;
     }
 }
